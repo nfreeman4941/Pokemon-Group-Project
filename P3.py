6dbc0bf107267d2b331ae000f36377a709b87317
nfreeman <nfreeman4941@gmail.com>
2026-03-27 12:22:06 -0600

    pokemon class

--- P3.py
+++ P3.py
@@ -1,2 +1,16 @@
 #Nina Freeman, Lydia McPhee, Rebeca Mousser, Mason Stewart
 #Pokemon Battle
+
+class Pokemon:
+    def __init__(self,name,elemental_type,hit_points):
+        self.name = name
+        self.elemental_type = elemental_type
+        self.hit_points = hit_points
+
+    def get_info(self):
+        return f'{self.name.title()} - Type: {self.elemental_type} - Hit Points: {self.hit_points}'
+    
+    def heal(self):
+        self.hit_points += 15
+
+        return f'{self.name.title()} has been healed to {self.hit_points}'
